ae78fa13ab63e8b8b83efaa6f49a53478832d0ca
wggly <157278738+wggly@users.noreply.github.com>
2024-01-21 19:51:48 +0700

    Add files via upload

---
+++ files/cf.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#random
+apt install jq curl -y
+rm -rf /root/xray/scdomain
+mkdir -p /root/xray
+MYIP=$(curl -sS ipv4.icanhazip.com)
+clear
+echo ""
+echo ""
+echo ""
+#sub=$(</dev/urandom tr -dc a-z0-9 | head -c3)
+read -rp "Input Domain Name. Example ( BogelStore ): " -e sub
+DOMAIN=mypremium.my.id
+SUB_DOMAIN=${sub}.mypremium.my.id
+CF_ID=bogelxdvpn@gmail.com
+CF_KEY=4af84b0309a1e1d70602126f50efcb7c8b4ed
+set -euo pipefail
+IP=$(curl -sS ipv4.icanhazip.com);
+echo "Updating DNS for ${SUB_DOMAIN}..."
+ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" | jq -r .result[0].id)
+if [[ "${#RECORD}" -le 10 ]]; then
+RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
+fi
+RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
+-H "X-Auth-Email: ${CF_ID}" \
+-H "X-Auth-Key: ${CF_KEY}" \
+-H "Content-Type: application/json" \
+--data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
+
+echo "$SUB_DOMAIN" > /root/domain
+echo "$SUB_DOMAIN" > /root/scdomain
+echo "$SUB_DOMAIN" > /etc/xray/domain
+echo "$SUB_DOMAIN" > /etc/v2ray/domain
+echo "$SUB_DOMAIN" > /etc/xray/scdomain
+echo "IP=$SUB_DOMAIN" > /var/lib/kyt/ipvps.conf
+rm -rf cf
+sleep 1
